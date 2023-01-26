5ef09ae2bb923b166b42edaa6d3edaabc4fbd45a
Jaysukhb <jaysukhbaldaniya10@gmail.com>
2023-01-26 15:38:04 +0530

    added public files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 
 module.exports = {
+  pathPrefix: "/yt1s",
   siteMetadata: {
     title: `YT1s.com`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
